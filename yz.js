ff781229e8fe4c6f36a3df0a9fdf8e5f9d2654e7
Aresy.z <yzsolo125@gmail.com>
2014-05-18 00:21:15 +0800

    tz

--- yz.js
+++ yz.js
@@ -13,8 +13,8 @@ window.onload = function () {
 		y_f = 1,
 		x = 100,
 		y = 100,
-		x_s = Math.random()*1,
-		y_s = Math.random()*1;
+		x_s = Math.random()*1/2,
+		y_s = Math.random()*1/2;
 	function move() {
 		can.width = window.innerWidth;
 		can.height = window.innerHeight;
@@ -36,6 +36,17 @@ window.onload = function () {
 		con.lineWidth = 4;
 		con.arc(x,y,2,0,2*Math.PI);
 		con.stroke();
-	}
-	setInterval(move,20);
+
+			}
+	// var a_f = 0.1;
+	// var a_f = setInterval(function() {
+	// 	if(con.globalAlpha==1) {
+	// 		a_f = -0.1;
+	// 	}else if (con.globalAlpha==0) {
+	// 		a_f = 0.1;
+	// 	}
+	// 	con.globalAlpha = con.globalAlpha + a_f;
+	// 	console.log(con.globalAlpha);
+	// },1000)
+	setInterval(move,50);
 }
