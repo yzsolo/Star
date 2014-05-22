80d32ac202f8e8cc7e86374b6b5f3e8cb9bd25d5
Aresy.z <yzsolo125@gmail.com>
2014-05-22 20:48:54 +0800

    js 重构

--- yz_star.js
+++ yz_star.js
@@ -6,89 +6,66 @@
 
 
 window.onload = function () {
+	var star = [];
 	var can = document.getElementById('yz_canvas');
 	can.width = window.innerWidth;
 	can.height = window.innerHeight;
 	var con = can.getContext('2d');
+	var x,y,x_s,y_s,x_f=1,y_f=1,stars = [],line_width=2,star_num=50;
 
-	var Circle = function () {
-		
+	for (var i = 0; i < star_num; i ++) {
+		x = (window.innerWidth-5)*Math.random(),
+		y = (window.innerHeight-5)*Math.random(),
+		x_s = Math.random()*1/2,
+		y_s = Math.random()*1/2;
+		option = {
+			x:x,
+			y:y,
+			x_s:x_s,
+			y_s:y_s,
+			x_f:x_f,
+			y_f:y_f,
+			line_width:line_width
+		}
+		stars.push(option);
 	}
-	
-	Circle.prototype = {
 
-		init : function() {
-			var	x_f = 1,
-			y_f = 1,
-			line_width = 2,
-			x = (window.innerWidth-5)*Math.random(),
-			y = (window.innerHeight-5)*Math.random(),
-			x_s = Math.random()*1/2,
-			y_s = Math.random()*1/2;
-			return {
-				x: x,
-				y: y,
-				x_s: x_s,
-				y_s: y_s,
-				x_f: x_f,
-				y_f: y_f,
-				line_width: line_width
-			}
-		},
-		draw : function () {
-			var init = this.init();
+	function draw () {
+		can.width = window.innerWidth;
+		can.height = window.innerHeight;
+		con.clearRect(0,0,can.width,can.height);
+		for (var j = 0; j < star_num; j ++) {
 			con.beginPath();
 			con.fillStyle = '#fff';
 			con.strokeStyle = '#fff';
 			con.lineWidth = 2;
-			con.arc(init.x,init.y,1,0,2*Math.PI);
+			con.arc(stars[j].x,stars[j].y,1,0,2*Math.PI);
 			con.stroke();
-			var option = {
-				x: init.x,
-				y: init.y,
-				x_s: init.x_s,
-				y_s: init.y_s,
-				x_f: init.x_f,
-				y_f: init.y_f,
-				line_width: init.line_width
+			
+			if (stars[j].x > window.innerWidth - 2 || stars[j].x < 2) {
+				stars[j].x_f *= -1;
 			}
-			return option;
-		},
-		move : function () {
-			var draw = this.draw();
-			can.width = window.innerWidth;
-			can.height = window.innerHeight;
-			function m () {
-				con.clearRect(draw.x-5,draw.y-5,10,10);
-				if(draw.x>window.innerWidth-2 || draw.x<2) {
-					draw.x_f *= -1;
-				}
-				if(draw.y>window.innerHeight-2 || draw.y<2) {
-					draw.y_f *= -1;
-				}
-				draw.x += draw.x_s*draw.x_f;
-				draw.y += draw.y_s*draw.y_f;
 
-				
-				con.beginPath();
-				con.fillStyle = '#fff';
-				con.strokeStyle = '#fff';
-				con.lineWidth = 2;
-				con.arc(draw.x,draw.y,1,0,2*Math.PI);
-				con.stroke();
+			if (stars[j].y > window.innerHeight - 2 || stars[j].y < 2) {
+				stars[j].y_f *= -1;
 			}
-			setInterval(m,50);
+			stars[j].x += stars[j].x_s*stars[j].x_f;
+			stars[j].y += stars[j].y_s*stars[j].y_f;
 		}
-
 	}
 
-	var star = [];
+	function drawLoop () {
+		draw();
+		//wait to finish
+		// can.onmousemove = function () {
+		// 	for (var i = 0; i < star_num; i++) {
+		// 		if ((stars[i].x-10<=event.clientX<=stars[i].x+10)&&(stars[i].y-10<=event.clientY<=stars[i].y+10)) {
+		// 			console.log('ha');
+		// 		}
+		// 	}
+		// }
+		setTimeout(drawLoop,50);
+	}
 
-	for(var i=0;i<50;i++) {
-
-			star[i] = new Circle();
-			star[i].draw();
-			setTimeout(star[i].move(),1000);	
-
-	}	
+	drawLoop();
 }
