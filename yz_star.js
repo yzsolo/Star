dde18385cb216668c58b2d67be40297fb98891b9
Aresy.z <yzsolo125@gmail.com>
2014-05-23 15:41:18 +0800

    add the line

--- yz_star.js
+++ yz_star.js
@@ -11,23 +11,26 @@ window.onload = function () {
 	can.width = window.innerWidth;
 	can.height = window.innerHeight;
 	var con = can.getContext('2d');
-	var x,y,x_s,y_s,x_f=1,y_f=1,stars = [],line_width=2,star_num=50;
+	var x,y,x_s,y_s,x_f=1,y_f=1,stars = [],line_width=0.1,star_num=30;
 
 	for (var i = 0; i < star_num; i ++) {
 		x = (window.innerWidth-5)*Math.random(),
 		y = (window.innerHeight-5)*Math.random(),
-		x_s = Math.random()*1/2,
-		y_s = Math.random()*1/2;
+		x_s = Math.random()*1/2-0.2,
+		y_s = Math.random()*1/2-0.2;
 		option = {
 			x:x,
 			y:y,
 			x_s:x_s,
 			y_s:y_s,
 			x_f:x_f,
-			y_f:y_f,
-			line_width:line_width
+			y_f:y_f
 		}
 		stars.push(option);
+	}
+
+	function draw_circle (j) {
+		
 	}
 
 	function draw () {
@@ -35,12 +38,12 @@ window.onload = function () {
 		can.height = window.innerHeight;
 		con.clearRect(0,0,can.width,can.height);
 		for (var j = 0; j < star_num; j ++) {
+			
 			con.beginPath();
 			con.fillStyle = '#fff';
-			con.strokeStyle = '#fff';
-			con.lineWidth = 2;
-			con.arc(stars[j].x,stars[j].y,1,0,2*Math.PI);
-			con.stroke();
+			con.arc(stars[j].x,stars[j].y,2.5,0,2 * Math.PI);
+			con.closePath();
+			con.fill();
 			
 			if (stars[j].x > window.innerWidth - 2 || stars[j].x < 2) {
 				stars[j].x_f *= -1;
@@ -49,22 +52,29 @@ window.onload = function () {
 			if (stars[j].y > window.innerHeight - 2 || stars[j].y < 2) {
 				stars[j].y_f *= -1;
 			}
-			stars[j].x += stars[j].x_s*stars[j].x_f;
-			stars[j].y += stars[j].y_s*stars[j].y_f;
+			stars[j].x += stars[j].x_s * stars[j].x_f;
+			stars[j].y += stars[j].y_s * stars[j].y_f;
 		}
+
+		con.lineWidth = line_width;
+  		con.strokeStyle = '#fff';
+
+  		for (var k = 0; k < star_num; k ++) {	
+  			
+  			for (var n = 0; n < star_num; n ++) {
+  				con.beginPath();
+  				con.moveTo(stars[k].x,stars[k].y);
+  				con.lineTo(stars[n].x,stars[n].y);
+  				con.stroke();
+  				con.closePath();
+  			}
+
+  		}
 	}
 
 	function drawLoop () {
-		draw();
-		//wait to finish
-		// can.onmousemove = function () {
-		// 	for (var i = 0; i < star_num; i++) {
-		// 		if ((stars[i].x-10<=event.clientX<=stars[i].x+10)&&(stars[i].y-10<=event.clientY<=stars[i].y+10)) {
-		// 			console.log('ha');
-		// 		}
-		// 	}
-		// }
 		setTimeout(drawLoop,50);
+		draw();
 	}
 
 	drawLoop();
